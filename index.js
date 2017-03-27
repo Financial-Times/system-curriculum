6fa31b94c9b9062f34ebbca123d72efada5db041
Luke Blaney <luke.blaney@ft.com>
2017-03-27 12:45:48 +0100

    get list of systems for a given team.  Add placeholder pages

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express'),
-    bodyParser = require('body-parser'),
-    mustacheExpress = require('mustache-express'),
-    path = require('path'),
-    CMDB = require( "cmdb.js" ),
-    ftwebservice = require('express-ftwebservice');
+	bodyParser = require('body-parser'),
+	mustacheExpress = require('mustache-express'),
+	path = require('path'),
+	CMDB = require( "cmdb.js" ),
+	ftwebservice = require('express-ftwebservice');
+require('es6-promise').polyfill();
 
 /** Set up express app **/
 const app = express();
@@ -18,47 +19,47 @@ app.use(express.static('public'));
 /** Environment variables **/
 const port = process.env.PORT || 8080;
 const cmdb = new CMDB({
-    api: process.env.CMDB_API,
-    apikey: process.env.CMDB_APIKEY,
+	api: process.env.CMDB_API,
+	apikey: process.env.CMDB_APIKEY,
 });
 
 /** Setup standard FT endpoints **/
 ftwebservice(app, {
-    manifestPath: path.join(__dirname, 'package.json'),
-    about: {
-        "systemCode": "system-curriculum",
-        "name": "System Curriculum",
-        "audience": "FT Technology",
-        "serviceTier": "bronze",
-    },
+	manifestPath: path.join(__dirname, 'package.json'),
+	about: {
+		"systemCode": "system-curriculum",
+		"name": "System Curriculum",
+		"audience": "FT Technology",
+		"serviceTier": "bronze",
+	},
 
-    // Also pass good to go.  If application is healthy enough to return it, then it can serve traffic.
-    goodToGoTest: function() {
-        return new Promise(resolve => {
-            resolve(true);
-        });
-    },
+	// Also pass good to go.  If application is healthy enough to return it, then it can serve traffic.
+	goodToGoTest: function() {
+		return new Promise(resolve => {
+			resolve(true);
+		});
+	},
 
-    // Check that track can talk to CMDB
-    healthCheck: function() {
-        return cmdb.getItem(null, 'system', 'system-registry').then(result => {
-            return false;
-        }).catch(error => {
-            return error.message;
-        }).then(output => {
-             return [{
-                id: 'cmdb-connection',
-                name: "Connectivity to CMDB",
-                ok: !output,
-                severity: 1,
-                businessImpact: "Can't manage system information through the UI",
-                technicalSummary: "App can't connect make a GET request to CMDB",
-                panicGuide: "Check for alerts related to cmdb.ft.com.    Check connectivity to cmdb.ft.com",
-                checkOutput: output,
-                lastUpdated: new Date().toISOString(),
-            }];
-        });
-    }
+	// Check that track can talk to CMDB
+	healthCheck: function() {
+		return cmdb.getItem(null, 'system', 'system-registry').then(result => {
+			return false;
+		}).catch(error => {
+			return error.message;
+		}).then(output => {
+			 return [{
+				id: 'cmdb-connection',
+				name: "Connectivity to CMDB",
+				ok: !output,
+				severity: 1,
+				businessImpact: "Can't manage system information through the UI",
+				technicalSummary: "App can't connect make a GET request to CMDB",
+				panicGuide: "Check for alerts related to cmdb.ft.com.	Check connectivity to cmdb.ft.com",
+				checkOutput: output,
+				lastUpdated: new Date().toISOString(),
+			}];
+		});
+	}
 });
 
 // Add authentication to everything which isn't one of the standard ftwebservice paths
@@ -73,23 +74,66 @@ app.use(function(req, res, next) {
  * Gets a list of systems from the CMDB and renders them
  */
 app.get('/', (req, res) => {
-    res.render('index', {});
+	res.render('index', {});
+});
+
+/**
+ * Gets a list of systems from the CMDB and renders them
+ */
+app.get('/team/:teamid', (req, res) => {
+	getTeamSystems(res.locals, req.params.teamid).then(teamsystems => {
+		res.render('teamoverview', {
+			title: teamsystems.teamname + " Systems",
+			teamid: teamsystems.teamid,
+		});
+	}).catch(error => {
+		res.status(502);
+		res.render("error", {message: "Unable to read team from CMDB ("+error+")"});
+	});
+});
+
+app.get('/team/:teamid/form', (req, res) => {
+	getTeamSystems(res.locals, req.params.teamid).then(teamsystems => {
+		res.render('form', {
+			title: teamsystems.teamname + " Systems | Update Form"
+		});
+	}).catch(error => {
+		res.status(502);
+		res.render("error", {message: "Unable to read team from CMDB ("+error+")"});
+	});
+
 });
 
 app.use((req, res) => {
-    res.status(404).render('error', {message:"Page not found."});
+	res.status(404).render('error', {message:"Page not found."});
 });
 
 app.use((err, req, res) => {
-    console.error(err.stack);
-    res.status(500);
-    if (res.get('Content-Type') && res.get('Content-Type').indexOf("json") != -1) {
-        res.send({error: "Sorry, an unknown error occurred."});
-    } else {
-        res.render('error', {message:"Sorry, an unknown error occurred."});
-    }
+	console.error(err.stack);
+	res.status(500);
+	if (res.get('Content-Type') && res.get('Content-Type').indexOf("json") != -1) {
+		res.send({error: "Sorry, an unknown error occurred."});
+	} else {
+		res.render('error', {message:"Sorry, an unknown error occurred."});
+	}
 });
 
 app.listen(port, () => {
-    console.log('App listening on port '+port);
+	console.log('App listening on port '+port);
 });
+
+function getTeamSystems(reslocals, teamid) {
+	return cmdb.getItem(reslocals, 'contact', teamid).then(teamdata => {
+		var systemFetches = [];
+		teamdata.isSecondaryContactfor.system.forEach(system =>{
+			systemFetches.push(cmdb.getItem(reslocals, 'system', system.dataItemID));
+		});
+		return Promise.all(systemFetches).then(systems => {
+			return {
+				teamid: teamid,
+				teamname: teamdata.name,
+				systems: systems,
+			}
+		});
+	})
+}
